c3d0d6a8e6301b52d6f93c31be88fed947e4ccaa
Sinrock <mruhaak@protonmail.com>
2020-12-14 16:12:11 -0700

    commit tree de2350a1bd67cb796f5eac1f876bb51c1be4e31c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
-# app/controllers/posts_controller.rb
- 
-class PostsController < ApplicationController
+    # app/controllers/posts_controller.rb
+     
     def show
+      @post = Post.find(params[:id])
     end
-  end
